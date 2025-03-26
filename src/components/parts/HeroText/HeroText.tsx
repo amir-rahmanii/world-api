63f05a0c2f626e1145bd304d296c582bd87ad746
Amirreza Rahmani <amirbabble@gmail.com>
2025-03-26 03:52:39 +0330

    remove DotPatternWrapper

--- src/components/parts/HeroText/HeroText.tsx
+++ src/components/parts/HeroText/HeroText.tsx
@@ -2,12 +2,12 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 
-import DotPatternWrapper from "../DotPatternWrapper/DotPatternWrapper";
+// import DotPatternWrapper from "../DotPatternWrapper/DotPatternWrapper";
 
 export default function HeroText() {
   return (
     <div className="flex flex-col gap-8 justify-center  items-center h-[calc(100vh-220px)]  desktop:h-[calc(100vh-180px)]">
-      <DotPatternWrapper />
+      {/* <DotPatternWrapper /> */}
       <h1 className="text-3xl pb-0 tablet:text-4xl desktop:text-5xl text-center font-extrabold">
         وب سرویس رایگان اطلاعات <span className="text-primary">کشورها</span>
       </h1>
