ce04abb56f1808b9867c222ff258ad6392d474d4
Amirreza Rahmani <amirbabble@gmail.com>
2025-06-29 22:56:28 +0330

    fix(fa.json en.json): fix bug username

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from "@fullstacksjs/eslint-config";
-import * as regexpPlugin from "eslint-plugin-regexp";
 
-export default defineConfig(
-  {
-    typescript: {
-      tsconfigRootDir: import.meta.dirname,
-    },
+export default defineConfig({
+  typescript: {
+    tsconfigRootDir: import.meta.dirname,
   },
-  regexpPlugin.configs["flat/recommended"],
-);
+});
