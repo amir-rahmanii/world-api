08c03bbfe7f52b36720a471e566cae44fe279e13
Amirreza Rahmani <amirbabble@gmail.com>
2025-08-22 15:17:49 +0330

    fix: show avatar profile

--- src/components/parts/Profile/Avatar.tsx
+++ src/components/parts/Profile/Avatar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import TooltipWrapper from '@/components/shared/TooltipWrapper/TooltipWrapper';
 import { Button } from '@/components/ui/button';
-import { Link } from '@/i18n/navigation';
+import { redirect } from '@/i18n/navigation';
 
 interface AvatarProps {
   avatarUrl: string;
@@ -15,21 +15,19 @@ export default function Avatar({ avatarUrl, name, tooltip }: AvatarProps) {
   return (
     <TooltipWrapper content={tooltip}>
       <Button
-        asChild
         className="rounded-full p-1 tablet:rounded-md tablet:px-4 tablet:py-2"
         variant="outline"
+        onClick={() => redirect({ href: '/dashboard/requests', locale: 'fa' })}
       >
-        <Link href="/dashboard/requests">
-          <span className="hidden tablet:block">{name}</span>
-          <div className="h-7 w-7 overflow-hidden rounded-full">
-            <Image
-              height={220}
-              width={220}
-              alt="profile"
-              src={avatarUrl || '/default-avatar.jpg'}
-            />
-          </div>
-        </Link>
+        <span className="hidden tablet:block">{name}</span>
+        <div className="h-7 w-7 overflow-hidden rounded-full">
+          <Image
+            height={220}
+            width={220}
+            alt="profile"
+            src={avatarUrl || '/default-avatar.jpg'}
+          />
+        </div>
       </Button>
     </TooltipWrapper>
   );
